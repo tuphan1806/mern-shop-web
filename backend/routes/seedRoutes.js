83e60b637153977a1185421e8d5689c6c9d6e294
Tu <tuphan18062002@gmail.com>
2023-04-24 22:23:52 +0700

    loi 500 ?

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -5,7 +5,7 @@ import data from '../data.js';
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-  await Product.remove({});
+  await Product.deleteMany({});
   const createdProducts = await Product.insertMany(data.products);
   res.send({ createdProducts });
 });
