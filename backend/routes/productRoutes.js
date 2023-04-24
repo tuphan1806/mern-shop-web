d5830e4fe1b865c4d77eb03c02f58fcd1d3d1021
Tu <tuphan18062002@gmail.com>
2023-04-24 22:41:03 +0700

    Creat Sample Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+
 const productRouter = express.Router();
 
 productRouter.get('/', async (req, res) => {
@@ -15,9 +16,8 @@ productRouter.get('/slug/:slug', async (req, res) => {
     res.status(404).send({ message: 'Product Not Found' });
   }
 });
-
 productRouter.get('/:id', async (req, res) => {
-  const product = await Product.findByID(req.params.id);
+  const product = await Product.findById(req.params.id);
   if (product) {
     res.send(product);
   } else {
